4265c56a8301abc92484b53d24d8eb991a014b41
Denis <koprmalak212@gmail.com>
2025-02-20 14:39:57 +0100

    bezig met navbar
    yamai niet aanraken aub poekie

---
+++ blitter/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blitter</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<div class="navBar">
+    <a href="index.php"><img src="assets/Blitter.jpeg"  id="logo" alt="Logo/Home Page Button"</a>
+    <a href="#"><img src="assets/exploreIcon.jpg" alt=""></a>
+    <a href="#">Notifications</a>
+    <a href="#"><img src="assets/messagesIcon.png"  id="logo" alt="Messages Button"></a>
+    <a href="#">Bookmarks</a>
+    <a href="#">Profile</a>
+    <a href="#">More</a>
+    <a href="#" id="tweetButton" >Tweet</a>
+</div>
+
+</body>
+</html>
