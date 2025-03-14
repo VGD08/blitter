904acbe12990b053e7969d77b62f6e8070b0297c
Denis <koprmalak212@gmail.com>
2025-03-14 13:00:30 +0100

    t

--- blitter/index.php
+++ blitter/index.php
@@ -9,16 +9,10 @@
     <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
 </head>
 <body>
-<div class="navBar">
-    <a href="index.php"><img src="assets/Blitter.jpeg"  id="logo" alt="Logo/Home Page Button"</a>
-    <a href="#"><img src="assets/exploreIcon.jpg" alt=""></a>
-    <a href="#">Notifications</a>
-    <a href="#"><img src="assets/messagesIcon.png"  id="logo" alt="Messages Button"></a>
-    <a href="#">Bookmarks</a>
-    <a href="#">Profile</a>
-    <a href="#">More</a>
-    <a href="#" id="tweetButton" >Tweet</a>
-</div>
 
+<!--Login Page-->
+<div class="login-box">
+    </div>
+</div>
 </body>
 </html>
